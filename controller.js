fb692355b9912f8458e51f9e5102b0c168527c10
haadeest <destiohrdn.03@gmail.com>
2024-05-09 11:24:25 +0700

    berhasil menambahkan group matakuliah (nested)

--- controller.js
+++ controller.js
@@ -9,13 +9,14 @@ exports.index = function (req, res) {
 
 // menampilkan semua data mahasiswa
 exports.tampilsemuamahasiswa = function (req, res) {
-    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
-        if (error) {
-            console.log(error)
-        } else {
-            response.ok(rows, res)
-        }
-    });
+    connection.query('SELECT * FROM mahasiswa',
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error)
+            } else {
+                response.ok(rows, res)
+            }
+        });
 };
 
 // menampikan semua data mahasiswa berdasarkan id
@@ -64,18 +65,31 @@ exports.ubahMahasiswa = function (req, res) {
                 response.ok("Berhasil mengubah data!", res)
             }
         })
-} 
+}
 
 // menghapus mahasiswa berdasarkan id
 exports.hapusMahasiswa = function (req, res) {
     var id = req.body.id_mahasiswa;
 
-    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id], 
-    function(error, rows, fields) {
-        if(error){
-            console.log(error)
-        } else {
-            response.ok("Berhasil menghapus data!", res)
-        }
-    })
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error)
+            } else {
+                response.ok("Berhasil menghapus data!", res)
+            }
+        })
+}
+
+// menampilkan matakuliah group
+exports.tampilGroupMatakuliah = function (req, res) {
+    connection.query('SELECT mahasiswa.id_mahasiswa, mahasiswa.nim, mahasiswa.nama, mahasiswa.jurusan, matakuliah.matakuliah, matakuliah.sks FROM krs JOIN matakuliah JOIN mahasiswa WHERE krs.id_mahasiswa = mahasiswa.id_mahasiswa AND krs.id_matakuliah = matakuliah.id_matakuliah ORDER BY mahasiswa.id_mahasiswa',
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error)
+            } else {
+                response.okNested(rows, res)
+            }
+        })
+
 }
--- res.js
+++ res.js
@@ -6,6 +6,37 @@ exports.ok = function (values, res) {
         'values': values
     };
 
-     res.json(data);
-     res.end();
+    res.json(data);
+    res.end();
+}
+
+// response untuk nested matakuliah
+exports.okNested = function (values, res) {
+    // lakukan akumulasi
+    const hasil = values.reduce((akumulasikan, item) => {
+        // tentukan key grup
+        if (akumulasikan[item.nama]) {
+            // buat variabel grup nama mahasiswa
+            const group = akumulasikan[item.nama];
+            // cek jika isi array adalah matakuliah
+            if (Array.isArray(group.matakuliah)) {
+                // tambahkan value ke group matakuliah
+                group.matakuliah.push(item.matakuliah);
+            } else {
+                group.matakuliah = [group.matakuliah, item.matakuliah];
+            }
+        } else {
+            akumulasikan[item.nama] = item;
+        }
+
+        return akumulasikan;
+    }, {});
+
+    var data = {
+        'status': 200,
+        'values': hasil
+    };
+
+    res.json(data);
+    res.end();
 }
